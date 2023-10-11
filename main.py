a23a52f5238c7450055f6e20654cb4cd9d010887
FoskyM <i@fosky.top>
2023-10-11 22:41:21 +0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,48 @@
+def merge_list(la, lb):
+
+
+class LNode:
+    def __init__(self, data=None):
+        self.data = data  # 结点的数据域
+        self.next = None  # 结点的指针域
+
+class LinkList:
+    def __init__(self):
+        # 生成新结点作为头结点并初始化指针域和数据区域为None，头指针head指向头节点
+        self.head = LNode(None)
+
+    def __iter__(self):
+        p = self.head
+        while p is not None:
+            yield p
+            p = p.next
+
+    def create_list_r(self, l_data: list):
+        # 后插法，根据l_data数据列表创建链表
+        r = self.head  # 尾指针r指向头结点
+        for data in l_data:
+            p = LNode(data)  # 生成新结点，并初始化p的数据域为data
+            r.next = p  # 将新结点p插入尾结点r之后
+            r = r.next  # r指向新的尾结点p
+
+    def show(self):
+        for p in self:
+            if p.data is not None:
+                print(p.data, end=' ')
+
+/ *请在这里填写答案 * /
+
+if __name__ == "__main__":
+    la = LinkList()
+    lb = LinkList()
+
+    a = input()
+    d1 = [int(x) for x in a.split()]
+    a = input()
+    d2 = [int(x) for x in a.split()]
+
+    la.create_list_r(d1)
+    lb.create_list_r(d2)
+    lc = merge_list(la, lb)
+
+    lc.show()
