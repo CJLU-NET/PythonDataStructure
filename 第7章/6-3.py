5f375f60425523695ac8191d7836bfa8fd3e91a2
FoskyM <i@fosky.top>
2023-12-21 21:56:19 +0800

    二叉排序树II

--- 第7章/6-3.py
+++ 第7章/6-3.py
@@ -1,13 +1,15 @@
 """
-6-2 二叉排序树-Python
-试写一个判别给定二叉树是否为二叉排序树的算法。
+6-3 二叉排序树II-Python
+已知二叉排序树采用二叉链表存储结构，根结点的指针为T，链结点的结构为（lchild,data,rchild），其中lchild，rchild分别指向该结点左、右孩子的指针，data域存放结点的数据信息。请写出递归算法，从小到大输出二叉排序树中所有数据值>=x的结点的数据。要求先找到第一个满足条件的结点后，再依次输出其他满足条件的结点。
+
 
 函数接口定义：
 在这里描述函数接口。例如：
-    def judge_bst(self):
+    def search_bst(self, x):
 
 输入样例：
 在这里给出一组输入。例如：
+    5
     3
     1
     #
@@ -22,16 +24,21 @@
     9
     #
     #
+    1
 
 输出样例：
 在这里给出相应的输出。例如：
-    Yes
+    1
+    3
+    4
+    5
+    6
+    7
+    9
 
 @Author: FoskyM
-@Date: 2023-12-21 20:27
+@Date: 2023-12-21 21:33
 """
-
-pre = None
 
 
 class BinaryTree:
@@ -53,26 +60,20 @@ class BinaryTree:
             self.rchild = BinaryTree()  # 为当前结点创建一个空的右子树
             self.rchild.create_bitree()  # 递归创建右子树
 
-    def judge_bst(self):
-        global pre
+    def search_bst(self, x):
         if self.data is None:
-            return True
-        if not self.lchild.judge_bst():
-            return False
-        if pre is not None and self.data <= pre.data:
-            return False
-        pre = self
-        if not self.rchild.judge_bst():
-            return False
-        return True
+            return
+        if self.data >= x:
+            self.lchild.search_bst(x)
+            print(self.data)
+            self.rchild.search_bst(x)
+        else:
+            self.rchild.search_bst(x)
 
 
 if __name__ == '__main__':
-
     bt = BinaryTree()
     bt.create_bitree()
 
-    if bt.judge_bst():
-        print("Yes")
-    else:
-        print("No")
+    x = input()
+    bt.search_bst(x)
